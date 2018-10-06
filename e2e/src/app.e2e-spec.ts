c40b87b0e77c777cbc2b45acbe6da9ce46936d03
Robin Borjesson <robin.b@outlook.com>
2018-10-06 17:37:57 +0200

    Add step definitions for finding header and filling in data in fields

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -3,12 +3,25 @@ import { AppPage } from './app.po';
 describe('workspace-project App', () => {
   let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
+  it('should display create contact header', () => {
+    expect(page.getParagraphText()).toEqual('Create contact');
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular-address-book!');
+  it('should add a new contact', () => {
+    let count = page.cardCount();
+    expect(count).toBe(0);
+
+    page.getField('name').sendKeys('John Doe');
+    page.getField('email').sendKeys('john@craftacademy.se');
+    page.getField('company').sendKeys('Craft Academy');
+    page.getField('role').sendKeys('Tester');
+    page.getField('twitter').sendKeys('@tester');
+    page.getField('location').sendKeys('Gothenburg');
+    page.getField('notes').sendKeys('Some tester guy');
+
+    page.getSubmitButton().click();
+
+    count = page.cardCount();
+    expect(count).toBe(1);
   });
 });
